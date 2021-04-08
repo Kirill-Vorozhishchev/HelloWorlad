43e76f1d90f9006557834e1c553f4d8bd6caabe9
Kirill Vorozhishchev <1507kirill1998@mail.ru>
2021-04-08 17:41:09 +0700

    Add project files.

---
+++ HelloWorlad.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+// This function prints
+void print()
+{
+    // Вызываем оператор << на объект типа std::cout
+    std::cout << "Hello Skillbox!\n";
+}
+/*
+* 1
+* 2
+* 3
+* 4
+*/
+ int main()
+{
+
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+}
